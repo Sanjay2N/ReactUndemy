51d789a617c8c354a3f93eaaad76698cbcc3270a
sanjay2N <sanjaykcbcs@gmail.com>
2024-02-19 08:52:48 +0530

    dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
-function ExpenseItem() {
+import "./ExpenseItems.css";
+
+function ExpenseItem(prop) {
+  console.log(prop);
+  const { date, amount, title } = prop.expense;
   return (
-    <div>
-      <p>Expenses...</p>
+    <div className="expense-item">
+      <div>{date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
+      </div>
     </div>
   );
 }
